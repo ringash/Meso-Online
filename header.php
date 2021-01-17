b12f0b680ff027a735c99452ff84f7c72e503a10
Erine Gashi <erinangashi@gmail.com>
2021-01-17 16:38:22 +0100

    Krijova header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <div id="header-logo">
+        <a href="dashboard.html" alt="dashboard">
+            <img alt="logo" src="images/main/laptop-logo.png" />
+        </a>
+    </div>
+    <div id="header-list">
+        <a href="courses.html">KURSET</a>
+        <a href="about-us.html">RRETH NESH</a>
+        <a href="contact.html">KONTAKTI</a>
+    </div>
+    <div id="login-register">
+        <a href="login.html">KY&#199U</a>
+        <a href="register.html">REGJISTROHU</a>
+    </div>
+</body>
+
+</html>
