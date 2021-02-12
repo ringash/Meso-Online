2413dfc2ae92425f6c7dbc6671fd5657ad425fd2
Erine Gashi <erinangashi@gmail.com>
2021-02-12 15:12:10 +0100

    klasa simpleUser dhe databaseConfig done

---
+++ Logics/simpleUser.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'User.php';
+
+class SimpleUser extends User
+{
+    public function __construct($username,  $password, $role)
+    {
+        parent::__construct($username, $password, $role);
+        
+    }
+
+    public function setSession()
+    {
+        $_SESSION["role"] = "0";
+        $_SESSION['roleName'] = "SimpleUser";
+    }
+
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+  
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
