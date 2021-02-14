832e699442ef0319c868384533d21b303f4d332f
arita berisha <aritaberisha0@gmail.com>
2021-02-14 13:29:40 +0100

    organizim ne folders

---
+++ Views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Convergence' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/main-style.css" />
+</head>
+
+<body>
+    <div id="header-logo">
+        <a href="dashboard.php" alt="dashboard">
+            <img alt="logo" src="../images/main/laptop-logo.png" />
+        </a>
+    </div>
+    <div id="header-list">
+        <a href="courses.php">KURSET</a>
+        <a href="about-us.php">RRETH NESH</a>
+        <a href="contact.php">KONTAKTI</a>
+        <a href="settings.php">SETTINGS</a>
+    </div>
+    <div id="login-register">
+        <a href="login.php">KY&#199U</a>
+        <a href="register.php">REGJISTROHU</a>
+    </div>
+</body>
+
+</html>
